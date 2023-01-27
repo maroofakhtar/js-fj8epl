2f11d2bbfd81b4e52a027f775c84be686f74e35c
maroofakhtar <121049832+maroofakhtar@users.noreply.github.com>
2023-01-27 19:45:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fj8epl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fj8epl)
---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data(){
+    return{
+      title:' life is a test',
+      author:' Maroof',
+      age: 20
+    }
+  }
+})
+app.mount('#app')
